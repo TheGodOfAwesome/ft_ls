4551a9f42fa57f97b24fd17625dba45f8db5eef7
Kuzivakwashe MUVEZWA <kmuvezwa@e4r2p1.wtc.co.za>
2017-08-25 10:23:10 +0200

    added libft.h a C library needed for this code

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmuvezwa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/11 16:14:34 by kmuvezwa          #+#    #+#             */
+/*   Updated: 2017/08/07 10:43:16 by kmuvezwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && (s1[i] == s2[i]) && i < n)
+		i++;
+	return ((const unsigned char)s1[i] - (const unsigned char)s2[i]);
+}
